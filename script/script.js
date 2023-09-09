e04171081da9e9f38da6272dce2c43f192a20418
toolninja <144477909+toolninja@users.noreply.github.com>
2023-09-09 20:09:56 +0530

    Update script.js
    menu script added

--- script/script.js
+++ script/script.js
@@ -1,1 +1,6 @@
+const hamburgerMenuContainer = document.querySelector('.hamburger-menu-container');
+const headerNav = document.querySelector('.header-nav');
 
+hamburgerMenuContainer.addEventListener('click', () => {
+    hamburgerMenuContainer.classList.toggle('active');
+});
