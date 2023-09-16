74c26a97b5657e4ebe5de98fc643a552a366f2dd
toolninja <144477909+toolninja@users.noreply.github.com>
2023-09-16 12:01:55 +0530

    Update breadcrumb.js

--- script/breadcrumb.js
+++ script/breadcrumb.js
@@ -1,23 +1,24 @@
 var breadcrumbMapping = {
-    "https://toolninja.github.io/": "Home",
-    "https://toolninja.github.io/blog.html": "Blog",
-    "https://toolninja.github.io/faq-schema-generator.html": "FAQ Schema Generator",
-    "https://toolninja.github.io/html-table-generator.html": "HTML Table Generator",
-    "https://toolninja.github.io/about.html": "About",
-    "https://toolninja.github.io/faq-schema-generator-user-guide.html": "FAQ Schema Generator User Guide",
-    "https://toolninja.github.io/html-table-generator-user-guide.html": "HTML Table Generator User Guide"
-  };
-  
-  var currentPageUrl = window.location.href;
-  
-  var breadcrumbItems = [];
-  
-  for (var url in breadcrumbMapping) {
-    if (currentPageUrl.indexOf(url) === 0) {
-      breadcrumbItems.push({ name: breadcrumbMapping[url], url: url });
-    }
+  "https://toolninja.github.io/": "Home",
+  "https://toolninja.github.io/blog.html": "Blog",
+  "https://toolninja.github.io/faq-schema-generator.html": "FAQ Schema Generator",
+  "https://toolninja.github.io/html-table-generator.html": "HTML Table Generator",
+  "https://toolninja.github.io/about.html": "About",
+  "https://toolninja.github.io/faq-schema-generator-user-guide.html": "FAQ Schema Generator User Guide",
+  "https://toolninja.github.io/html-table-generator-user-guide.html": "HTML Table Generator User Guide"
+};
+
+var currentPageUrl = window.location.href;
+
+var breadcrumbItems = [];
+
+for (var url in breadcrumbMapping) {
+  if (currentPageUrl.indexOf(url) === 0) {
+    breadcrumbItems.push({ name: breadcrumbMapping[url], url: url });
   }
-  
+}
+
+if (breadcrumbItems.length > 0) {
   var breadcrumbSchema = {
     "@context": "http://schema.org",
     "@type": "BreadcrumbList",
@@ -30,10 +31,13 @@ var breadcrumbMapping = {
       };
     })
   };
-  
+
   var script = document.createElement("script");
   script.type = "application/ld+json";
   script.textContent = JSON.stringify(breadcrumbSchema);
-  
+
+  // Add the breadcrumb schema markup to the page's <head> section
   document.head.appendChild(script);
-  
+} else {
+  console.error("No matching breadcrumb items found for the current page URL.");
+}
