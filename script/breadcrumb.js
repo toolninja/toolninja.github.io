035ece4ba1d73314610871209f35ab0be980ff28
toolninja <144477909+toolninja@users.noreply.github.com>
2023-09-16 11:22:53 +0530

    Create breadcrumb.js

---
+++ script/breadcrumb.js
@@ -0,0 +1,39 @@
+var breadcrumbMapping = {
+    "https://toolninja.github.io/": "Home",
+    "https://toolninja.github.io/blog.html": "Blog",
+    "https://toolninja.github.io/faq-schema-generator.html": "FAQ Schema Generator",
+    "https://toolninja.github.io/html-table-generator.html": "HTML Table Generator",
+    "https://toolninja.github.io/about.html": "About",
+    "https://toolninja.github.io/faq-schema-generator-user-guide.html": "FAQ Schema Generator User Guide",
+    "https://toolninja.github.io/html-table-generator-user-guide.html": "HTML Table Generator User Guide"
+  };
+  
+  var currentPageUrl = window.location.href;
+  
+  var breadcrumbItems = [];
+  
+  for (var url in breadcrumbMapping) {
+    if (currentPageUrl.indexOf(url) === 0) {
+      breadcrumbItems.push({ name: breadcrumbMapping[url], url: url });
+    }
+  }
+  
+  var breadcrumbSchema = {
+    "@context": "http://schema.org",
+    "@type": "BreadcrumbList",
+    "itemListElement": breadcrumbItems.map(function (item, index) {
+      return {
+        "@type": "ListItem",
+        "position": index + 1,
+        "name": item.name,
+        "item": item.url
+      };
+    })
+  };
+  
+  var script = document.createElement("script");
+  script.type = "application/ld+json";
+  script.textContent = JSON.stringify(breadcrumbSchema);
+  
+  document.head.appendChild(script);
+  
