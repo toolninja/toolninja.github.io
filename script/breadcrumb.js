c431771eb4af873cc35fa377bfbaded984df9843
toolninja <144477909+toolninja@users.noreply.github.com>
2023-09-16 13:04:32 +0530

    Update breadcrumb.js

--- script/breadcrumb.js
+++ script/breadcrumb.js
@@ -1,43 +1,33 @@
-var breadcrumbMapping = {
-  "https://toolninja.github.io/": "Home",
-  "https://toolninja.github.io/blog.html": "Blog",
-  "https://toolninja.github.io/faq-schema-generator.html": "FAQ Schema Generator",
-  "https://toolninja.github.io/html-table-generator.html": "HTML Table Generator",
-  "https://toolninja.github.io/about.html": "About",
-  "https://toolninja.github.io/faq-schema-generator-user-guide.html": "FAQ Schema Generator User Guide",
-  "https://toolninja.github.io/html-table-generator-user-guide.html": "HTML Table Generator User Guide"
-};
+var currentPageURL = window.location.href;
 
-var currentPageUrl = window.location.href;
-
-var breadcrumbItems = [];
-
-for (var url in breadcrumbMapping) {
-  if (currentPageUrl.indexOf(url) === 0) {
-    breadcrumbItems.push({ name: breadcrumbMapping[url], url: url });
-  }
-}
-
-if (breadcrumbItems.length > 0) {
-  var breadcrumbSchema = {
+function createBreadcrumbMarkup() {
+  var breadcrumbList = {
     "@context": "http://schema.org",
     "@type": "BreadcrumbList",
-    "itemListElement": breadcrumbItems.map(function (item, index) {
-      return {
-        "@type": "ListItem",
-        "position": index + 1,
-        "name": item.name,
-        "item": item.url
-      };
-    })
+    "itemListElement": []
   };
 
-  var script = document.createElement("script");
-  script.type = "application/ld+json";
-  script.textContent = JSON.stringify(breadcrumbSchema);
+  var urlParts = currentPageURL.split('/');
 
-  // Add the breadcrumb schema markup to the page's <head> section
+  var currentURL = '';
+
+  for (var i = 1; i < urlParts.length; i++) {
+    currentURL += '/' + urlParts[i];
+    var breadcrumbItem = {
+      "@type": "ListItem",
+      "position": i,
+      "name": urlParts[i],
+      "item": "https://toolninja.github.io" + currentURL
+    };
+    breadcrumbList.itemListElement.push(breadcrumbItem);
+  }
+
+  
+  var script = document.createElement('script');
+  script.type = 'application/ld+json';
+  script.innerHTML = JSON.stringify(breadcrumbList);
+
   document.head.appendChild(script);
-} else {
-  console.error("No matching breadcrumb items found for the current page URL.");
 }
+
+createBreadcrumbMarkup();
